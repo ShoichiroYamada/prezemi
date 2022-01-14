f5f89a893f3c417ff0a4c760be8c956438324f37
山田　章一朗 <97659835+ShoichiroYamada@users.noreply.github.com>
2022-01-14 17:56:31 +0900

    fix: Change the number of trials
    Change from 100 times to 200 times.

--- prezemi1.py
+++ prezemi1.py
@@ -1,5 +1,7 @@
 
 for num in range(1,101):
++    
+for num in range(1,201):
     if num % 15 ==0:
       print("だいがく爆破したい")
     elif num % 5 ==0:
@@ -8,7 +10,8 @@ for num in range(1,101):
      print("勉強したい")
     else:
      print(num)
-     
+ +
+print(num)
 
 
     
